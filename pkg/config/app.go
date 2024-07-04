cce46313e5bff45506d6c754422008e670ad4a9d
abir835 <abirhasan.raj.bd@gmail.com>
2024-07-04 16:33:55 +0600

    setup controller and .env and database connection

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,1 +1,31 @@
 package config
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mssql"
+	"os"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	dbUser := os.Getenv("USERNAME")
+	dbPass := os.Getenv("PASSWORD")
+	dbName := os.Getenv("DATABASE")
+
+	dsn := fmt.Sprintf("%s:%s/%s?charset=utf8&parseTime=True&loc=Local",
+		dbUser, dbPass, dbName)
+
+	d, err := gorm.Open("mssql", dsn)
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
